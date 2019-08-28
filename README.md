02a866c7bdd4602b31b0621109e3b1dea6504e45
Shi Y.M <y_shi@timedia.co.jp>
2019-08-28 11:20:49 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+## Introduction
+A script to install and update vim packages  
+which if your use [pathogen.vim](https://github.com/tpope/vim-pathogen) as your vim package manager
+
+
+## Usage
+
+1. ### Install  
+
+    **for first time**
+    1. set your path of bundle directory in config.json  
+    1. set packages your need in config.json. Need Github url
+
+    **install packages**  
+    * ```vimenv install``` or just ```vimenv```
+
+1. ### update  
+
+    Just run ```vimenv update```
+
+hope this script will help you :-)
+
+bash version will coming soon
---
+++ vimenv.ps1
@@ -0,0 +1,23 @@
+Param($Method)
+
+$configData = Get-Content .\config.json | ConvertFrom-Json
+
+Set-Location $configData.bundlePath
+
+if ($Method -eq "update") {
+    $installedPackages = Get-ChildItem * | Where-Object { $_.PSIsContainer }
+    foreach($package in $installedPackages.Name) {
+        Set-Location $package
+        git pull
+        Set-Location "..\"
+    }
+}
+
+if ($Method -eq "install" -or $null -eq $Method) {
+    foreach ($package in $configData.packages) {
+        $packageName = $package.Split("/")[-1]
+        if (!(Test-Path -Path $packageName)) {
+            git clone $package
+        }
+    }
+}
